4ff54bd2bcb3b2e0bc7601ab4b9270eefbc5ab05
Abraão Garcia da Silva <abraao.silva@benner.com.br>
2026-08-15 17:00:11 -0300

    first commit

---
+++ Musica.cs
@@ -0,0 +1,33 @@
+﻿
+class Musica
+{
+    public string Nome { get; set; }
+    public string Artista  { get; set; }
+    public int Duracion  { get; set; }
+    public bool Disponivel { get; set; }
+
+    public string DescricaoResumida => $"A musica {Nome} pertence a banda {Artista}";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da Musica: {Nome}");
+        Console.WriteLine($"Nome do Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracion}");
+        if (Disponivel == true)
+        {
+            Console.WriteLine($"Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine($"Adquira o plano Plus+");
+        }
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {Nome} - {Artista}");
+    }
+
+    // Funcao Lanbda 
+    public int Somar(int a, int b) => a + b;
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Musica musica1 = new Musica();
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracion = 273;
+musica1.Disponivel  = true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracion = 373;
+musica2.Disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+musica1.ExibirNomeEArtista();
