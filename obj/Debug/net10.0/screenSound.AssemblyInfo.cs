18f3a5ac6ecc74c9442cbe25c7a6de220213d07e
Abraão Garcia da Silva <abraao.silva@benner.com.br>
2026-09-16 17:53:21 -0300

    add arquivos

--- obj/Debug/net10.0/screenSound.AssemblyInfo.cs
+++ obj/Debug/net10.0/screenSound.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("screenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+953aa28dae9405b4fbf01cbc8899e9bd0182665d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79e36849178051a977c05451fbb94b62d687fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("screenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("screenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
